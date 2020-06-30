a08d391a67c047f8b7c30c3f9d951859de492c3f
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2020-06-30 01:14:58 -0300

    Constrains going...

--- src/util/options.js
+++ src/util/options.js
@@ -1,3 +1,8 @@
+const {
+    question,
+    keyInSelect
+} = require('readline-sync');
+
 const defaultOptions = {
     version: 'string',
     method: 'string',
@@ -30,5 +35,48 @@ const options = {
 
 module.exports = function (request) {
     const opt = options[request.type];
-    console.log(opt);
+    request.options = {};
+    for (const key in opt) {
+        const r = askType(opt[key], `Insert the ${key}: `);
+        if (r === null) {
+            continue;
+        }
+        request.options[key] = r;
+    }
+}
+
+function askType(type, query) {
+    let _type = type,
+        _query = query,
+        required = false;
+
+    if (type.charAt(0) === '!') {
+        _type = type.substr(1);
+        _query += ' [REQUIRED] ';
+        required = true;
+    }
+
+    if (_type === 'boolean') {
+        
+    } else if (_type.includes('|')) {
+        const opt = _type.split('|');
+        let first = true;
+        let index;
+        do {
+            if (!first) console.log('Invalid input!');
+            index = keyInSelect(opt, _query);
+        } while (index !== -1);
+        return opt[index];
+    } else if (_type === 'function') {
+
+    } else {
+        let r;
+        let first = true;
+        let r;
+        do {
+            if (!first) console.log('Invalid input!');
+            r = question(_query);
+        } while (!r);
+        return r;
+    }
 }
