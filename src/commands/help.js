de3bb7c512d1f860620e11e04c990448225c7f5f
Gianarlo Fontela da Luz <gian.f.luz@hotmail.com>
2020-07-01 15:51:17 -0300

    Help.json update and fix on command help

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,12 +1,7 @@
 const data = require('../util/help.json');
 
 module.exports = function (args) {
-    let info;
-    try {
-        info = navigate(data, [...args]);
-    } catch (err) {
-        return 'Error: Command not founded';
-    }
+    const info = navigate(data, [...args]);
 
     if (info === data) {
         return `--{ gph }--\n` +
@@ -28,8 +23,8 @@ module.exports = function (args) {
 function navigate(object, arr) {
     if (typeof object !== 'object' || !arr.length) return object;
     const index = arr.shift();
-    if (!object[index] && !object.operations && !object.operations[index]) {
-        throw new Error('This command sequence does not exists');
+    if (!object[index] && (!object.operations || !object.operations[index])) {
+        return object;
     }
 
     return navigate(object[index] || object.operations[index], arr);
