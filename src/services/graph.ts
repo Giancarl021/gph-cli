e827fb435313b9107cffb6005be9d6f01859e352
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2022-08-03 11:24:31 -0300

    Added implementation of vault cache

--- src/services/graph.ts
+++ src/services/graph.ts
@@ -4,14 +4,14 @@ import GraphInterfaceDesktopProvider from 'graph-interface-desktop-provider';
 import { Credentials } from 'graph-interface/lib/interfaces';
 import { CommandInternal } from '@giancarl021/cli-core/interfaces';
 
-import hash from '../util/hash';
+import vaultCache from '../util/vault-cache';
 
 export default function (commandInternal: CommandInternal, credentials: Credentials, isDelegated: boolean, version?: string) {
-    const credentialsHash = hash(`${credentials.clientId}-${credentials.clientSecret}-${credentials.tenantId}::${isDelegated}`);
 
     const graph = GraphInterface(credentials, {
         authenticationProvider: isDelegated ? GraphInterfaceDesktopProvider({}) : undefined,
-        version
+        version,
+        cacheService: vaultCache(commandInternal, { auth: credentials, isDelegated })
     });
 
     return graph;
