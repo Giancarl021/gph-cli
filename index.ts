173867755f8844fefa41018987aad8f29229d40d
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2022-08-09 18:47:24 -0300

    Added help file

--- index.ts
+++ index.ts
@@ -1,17 +1,21 @@
 #!/usr/bin/env node
 
 import CliCore from '@giancarl021/cli-core';
+import { HelpDescriptor } from '@giancarl021/cli-core/interfaces';
 import { Behavior } from '@giancarl021/cli-core/interfaces';
 import CliCoreVaultExtension from '@giancarl021/cli-core-vault-extension';
 
 import vaultExtensionOptions from './src/util/vaultExtensionOptions';
 import * as commands from './src/commands';
+import help from './src/util/help.json';
 
 const APP_NAME = 'gph';
 const DEBUG_MODE = String(process.env.GPH_DEBUG).toLowerCase() === 'true';
 
 async function main() {
     const behavior: Behavior = {};
+    const _help: any = help;
+    delete _help['$schema'];
 
     if (DEBUG_MODE) {
         behavior.exitOnError = false;
@@ -27,6 +31,7 @@ async function main() {
         },
         behavior,
         commands,
+        help: _help as HelpDescriptor,
         extensions: [CliCoreVaultExtension(vaultExtensionOptions)]
     });
 
